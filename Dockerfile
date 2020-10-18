2b5fb4e192013b1aa82c659df0ad69f6ccdb70a4
kimocoder <christian@aircrack-ng.org>
2020-10-18 18:29:43 +0200

    Merge changes

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,12 @@
 FROM python:3.6
+RUN apt-get update && \
+    apt-get upgrade -y
 
 COPY requirements.txt /tmp/requirements.txt
-RUN python -m pip install -r /tmp/requirements.txt
+RUN pip install pip --upgrade
+RUN pip install -r /tmp/requirements.txt
 
 WORKDIR /routersploit
 COPY . .
 
-CMD ["python", "rsf.py"]
+#CMD ["python3", "/routersploit/rsf.py"]
--- rsf.py
+++ rsf.py
@@ -9,8 +9,11 @@ if sys.version_info.major < 3:
 
 from routersploit.interpreter import RoutersploitInterpreter
 
-log_handler = logging.handlers.RotatingFileHandler(filename="routersploit.log", maxBytes=500000)
-log_formatter = logging.Formatter("%(asctime)s %(levelname)s %(name)s       %(message)s")
+log_handler = logging.handlers.RotatingFileHandler(
+    filename="routersploit.log",
+    maxBytes=500000)
+log_formatter = logging.Formatter(
+    "%(asctime)s %(levelname)s %(name)s       %(message)s")
 log_handler.setFormatter(log_formatter)
 LOGGER = logging.getLogger()
 LOGGER.setLevel(logging.DEBUG)
